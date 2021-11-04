0961aae0d6cdc2b4790b4017c3c3c4ce205157f2
Odin34 <57544047+Odin34@users.noreply.github.com>
2021-11-04 09:25:32 -0600

    Added simple Spring Rest API to do CRUD operations on users

---
+++ backend/src/main/java/nakasone/odin/backend_translation/model/User.java
@@ -0,0 +1,38 @@
+package nakasone.odin.backend_translation.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "birthday")
+    private String birthday;
+
+    @Column(name = "profilePic")
+    @Nullable
+    private String profilePic;
+}
